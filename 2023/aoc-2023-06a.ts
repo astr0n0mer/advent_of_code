b5e5fcd52bfa71d1052eaf0d70206e7d87efd663
astr0n0mer <42691857+astr0n0mer@users.noreply.github.com>
2024-10-20 15:08:55 +0530

    feat: adds solution for 2023/12/6a

---
+++ 2023/aoc-2023-06a.ts
@@ -0,0 +1,51 @@
+
+const main = () => {
+  let INPUT = `
+Time:      7  15   30
+Distance:  9  40  200
+`;
+  INPUT = require("fs").readFileSync("./aoc-2023-06.txt", "utf-8");
+  console.log(new Array(100).fill("=").join(""));
+  const inputLines = INPUT.split("\n");
+  const [raceDurations, distanceRecords] = inputLines.filter(line => line.trim()).map((line) => line.split(/:\D+/)[1].split(/\s+/).map(Number));
+
+  const raceDurationToDistanceRecords = raceDurations.map((raceDuration, index) => ({ raceDuration, distanceRecord: distanceRecords[index] }));
+
+  const minAndMaxHoldDurations = raceDurationToDistanceRecords.map(({ raceDuration, distanceRecord }) => {
+    const halfRaceDuration = Math.ceil(raceDuration / 2);
+    let finalMinHoldDuration = Number.MAX_SAFE_INTEGER;
+    let finalMaxHoldDuration = Number.MIN_SAFE_INTEGER;
+
+    let minHoldDuration = 1;
+    let maxHoldDuration = halfRaceDuration;
+    while (minHoldDuration < maxHoldDuration) {
+      const midHoldDuration = Math.floor((minHoldDuration + maxHoldDuration) / 2);
+      const midHoldDistance = midHoldDuration * (raceDuration - midHoldDuration);
+
+      if (midHoldDistance > distanceRecord) {
+        finalMinHoldDuration = Math.min(finalMinHoldDuration, midHoldDuration);
+        finalMaxHoldDuration = Math.max(finalMaxHoldDuration, raceDuration - midHoldDuration);
+      }
+
+      if (midHoldDistance <= distanceRecord) {
+        if (minHoldDuration === midHoldDuration) {
+          break;
+        }
+        minHoldDuration = midHoldDuration;
+      } else if (midHoldDistance > distanceRecord) {
+        if (maxHoldDuration === midHoldDuration) {
+          break;
+        }
+        maxHoldDuration = midHoldDuration;
+      }
+    }
+
+    return { minHoldDuration: finalMinHoldDuration, maxHoldDuration: finalMaxHoldDuration };
+  });
+  console.debug(minAndMaxHoldDurations);
+
+  const numberOfWaysToBeatRecord = minAndMaxHoldDurations.reduce((acc, { minHoldDuration, maxHoldDuration }) => acc * (maxHoldDuration - minHoldDuration + 1), 1);
+  console.debug({ numberOfWaysToBeatRecord });
+}
+
+main();
